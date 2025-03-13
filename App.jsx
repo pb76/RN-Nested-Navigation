063b6f67b871a2a88235be7c2ccdcc54ceb1dd35
Patrik Bernhardsson <pb76@users.noreply.github.com>
2025-03-13 18:48:41 +0100

    Ordnat med titel och ikon för varje tab

--- App.jsx
+++ App.jsx
@@ -1,17 +1,38 @@
 import * as React from "react";
 import { Text, View } from "react-native";
 import { createStaticNavigation, useNavigation } from "@react-navigation/native";
-import { Button } from "@react-navigation/elements";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 import HomeScreen from "./Pages/Home";
 import AboutScreen from "./Pages/About";
 import JokesScreen from "./Pages/Jokes";
 
 const MyTabs = createBottomTabNavigator({
   screens: {
-    Home: HomeScreen,
-    Jokes: JokesScreen,
-    About: AboutScreen,
+    Home: {
+      screen: HomeScreen,
+      options: {
+        title: "Hem",
+        tabBarIcon: ({ focused }) => <Text style={{ color: focused ? "blue" : "black" }}>🏠</Text>,
+      },
+    },
+    Jokes: {
+      screen: JokesScreen,
+      options: {
+        title: "Skämt",
+        tabBarIcon: ({ focused }) => <Ionicons name="happy-outline" color={{ color: focused ? "blue" : "black" }} />,
+      },
+    },
+    About: {
+      screen: AboutScreen,
+      options: {
+        title: "Om",
+        tabBarIcon: ({ focused }) => (
+          <Ionicons name="information-circle-outline" color={{ color: focused ? "blue" : "black" }} />
+        ),
+      },
+    },
   },
 });
 
